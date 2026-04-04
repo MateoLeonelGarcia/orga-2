371fbfc1896d4d70820c44d1055a5f83a7cdfc53
Mateo Garcia <garoia2006@gmail.com>
2026-04-04 10:13:09 -0300

    ej 12 corregido

--- ClaseIntroductoriaC/Ejercicio12/ej12.c
+++ ClaseIntroductoriaC/Ejercicio12/ej12.c
@@ -5,28 +5,25 @@ int main(){
         
         uint32_t s[] = {1,2,3,4};
         uint32_t sCopia[] = {1,2,3,4};
-
-        // for (size_t i = 0; i < 4; i++)
-        // {
-        //         sCopia[i] = s[i];
-        // }
+        size_t sLength = sizeof(s)/sizeof(s[0]);
+        
         
         uint8_t rotacion = 2;
 
-        for (size_t i = 0; i < 4; i++){
+        for (size_t i = 0; i < sLength; i++){
                 printf("%i\n",s[i]);
         }
 
-        for (int32_t i = 0; i < 4; i++){
-                if ((i-rotacion) < 0){  
-                        s[i] = sCopia[4-rotacion+i];
-                } else {
-                        s[i] = sCopia[i-rotacion]; 
-                }                
+        for (size_t i = 0; i < sLength; i++){
+                if(i + rotacion < sLength){
+                        s[i] = sCopia[i+rotacion];
+                }else{
+                        s[i] = sCopia[i - sLength + rotacion];
+                }              
         }
         
 
-        for (size_t i = 0; i < 4; i++)
+        for (size_t i = 0; i < sLength; i++)
         {
                 printf("%i\n",s[i]);
         }
