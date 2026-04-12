2abe84da0a93d1994d89de775d98b03c8ee213f4
Mateo Garcia <garoia2006@gmail.com>
2026-04-12 14:12:00 -0300

    ej14

---
+++ avanzada/Ejercicio14/type.c
@@ -0,0 +1,45 @@
+#include "type.h"
+#include <stdlib.h>
+
+fat32_t *new_fat32()
+{
+    fat32_t *n = malloc(sizeof(fat32_t));
+    *n = 0;
+}
+ext4_t *new_ext4()
+{
+    ext4_t *n = malloc(sizeof(ext4_t));
+    *n = 0;
+}
+ntfs_t *new_ntfs()
+{
+    ntfs_t *n = malloc(sizeof(ntfs_t));
+    *n = 0;
+}
+fat32_t *copy_fat32(fat32_t *file)
+{
+    fat32_t *n = malloc(sizeof(fat32_t));
+    *n = *file;
+}
+ext4_t *copy_ext4(ext4_t *file)
+{
+    ext4_t *n = malloc(sizeof(ext4_t));
+    *n = *file;
+}
+ntfs_t *copy_ntfs(ntfs_t *file)
+{
+    ntfs_t *n = malloc(sizeof(ntfs_t));
+    *n = *file;
+}
+void rm_fat32(fat32_t *file)
+{
+    free(file);
+}
+void rm_ext4(ext4_t *file)
+{
+    free(file);
+}
+void rm_ntfs(ntfs_t *file)
+{
+    free(file);
+}
